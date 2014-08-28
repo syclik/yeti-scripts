f75648f71353354569d62d6ff66a586faef411ec
Daniel Lee <bearlee@alum.mit.edu>
2014-08-28 01:21:47 -0400

    adding scripts

---
+++ test-stan-develop.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+# 1. Update repository.
+echo '------------------------------------------------------------'
+echo 'Updating Stan repository to develop branch'
+
+pushd /vega/stats/users/dl2604/stan-develop
+## this takes a long time:
+# make clean-all
+
+git clean -d -x -f
+git checkout develop
+git pull --ff
+
+echo ''
+echo 'Branch information'
+git log -n 1
+
+echo 'Stan develop branch updated'
+echo ''
+
+echo '------------------------------------------------------------'
+
+qsub /u/9/d/dl2604/stan-test-scripts/qsub-stan-develop-setup.sh
