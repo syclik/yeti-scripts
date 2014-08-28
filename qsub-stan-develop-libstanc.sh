3e500000aefa15296adb11fde6711bb0397e382a
Daniel Lee <bearlee@alum.mit.edu>
2014-08-28 01:42:03 -0400

    updating scripts

--- qsub-stan-develop-libstanc.sh
+++ qsub-stan-develop-libstanc.sh
@@ -3,7 +3,7 @@
 # Directives
 #PBS -N stan-develop-libstanc
 #PBS -W group_list=yetistats
-#PBS -l nodes=1,walltime=00:05:00,mem=2gb
+#PBS -l nodes=1,walltime=00:04:00,mem=1gb
 #PBS -M dl2604@columbia.edu
 #PBS -m abe
 #PBS -V
