29b0315f4695e599225c3b87936b652f1a15cc6b
Daniel Lee <bearlee@alum.mit.edu>
2014-08-28 16:30:00 -0400

    update tests

--- qsub-libstan.sh
+++ qsub-libstan.sh
@@ -3,7 +3,7 @@
 : ${ID='develop'}
 
 # Directives
-#PBS -N $ID-stan-libstan
+#PBS -N ${ID}-stan-libstan
 #PBS -W group_list=yetistats
 #PBS -l nodes=1,walltime=00:02:00,mem=1gb
 #PBS -M dl2604@columbia.edu
