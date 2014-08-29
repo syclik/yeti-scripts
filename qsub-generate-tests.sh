415e6532b11b9e7ee9f54f026b582470418af88d
Daniel Lee <bearlee@alum.mit.edu>
2014-08-29 00:07:11 -0400

    manually generating tests

--- qsub-generate-tests.sh
+++ qsub-generate-tests.sh
@@ -20,13 +20,21 @@ echo 'Generating all tests'
 
 time make CC=clang++ test/unit-distribution/generate_tests
 
-targets=$(find src/test/unit-distribution -name '*_test.hpp' | sed 's|\(.*\)_test.hpp|\1_00000_generated_test.cpp|')
-
-echo 'targets: ' ${targets}
-for target in ${targets} 
+tests=$(find src/test/unit-distribution -name '*_test.hpp')
+for test in ${tests}
 do
-    make CC=clang++ ${target}
+    test/unit-distribution/generate_tests ${test}
 done
+echo 'tests: ' ${tests}
+
+
+# targets=$(find src/test/unit-distribution -name '*_test.hpp' | sed 's|\(.*\)_test.hpp|\1_00000_generated_test.cpp|')
+
+# echo 'targets: ' ${targets}
+# for target in ${targets} 
+# do
+#     make CC=clang++ ${target}
+# done
 
 echo ''
 echo 'Done building all generated test files'
