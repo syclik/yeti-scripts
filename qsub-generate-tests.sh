2f2ad5c26e0522ac3017055f7e90a6a8ca142f0e
Daniel Lee <bearlee@alum.mit.edu>
2014-08-28 23:57:20 -0400

    adding more time to generate-tests

--- qsub-generate-tests.sh
+++ qsub-generate-tests.sh
@@ -5,7 +5,7 @@
 # Directives
 #PBS -N ${ID}-stan-generate-tests
 #PBS -W group_list=yetistats
-#PBS -l nodes=1,walltime=00:02:00,mem=1gb
+#PBS -l nodes=1,walltime=00:10:00,mem=1gb
 #PBS -M dl2604@columbia.edu
 #PBS -m abe
 #PBS -V
