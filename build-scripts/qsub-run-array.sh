0cf1ecd4339cb5fc68a60a3a0e811b27cdffbfde
Daniel Lee <bearlee@alum.mit.edu>
2016-06-07 08:55:20 -0400

    updating

--- build-scripts/qsub-run-array.sh
+++ build-scripts/qsub-run-array.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 : {$STAN_PROGRAM=''}
-: {$STAN_PROGRAM_BASENAME=$(basename $STAN_PROGRAM)}
 : {$PROGRAM_ARGUMENTS=''}
 
 
@@ -17,13 +16,13 @@
 #PBS -o localhost:/vega/stats/users/dl2604/
 #PBS -e localhost:/vega/stats/users/dl2604/
 
-###STAN_PROGRAM_BASENAME=$(basename $STAN_PROGRAM)
+STAN_PROGRAM_BASENAME=$(basename $STAN_PROGRAM)
 
 
 echo '------------------------------------------------------------'
 echo 'Running'
 echo pwd: `pwd`
-echo $STAN_PROGRAM_BASENAME
+echo stan program basename: $STAN_PROGRAM_BASENAME
 echo time $STAN_PROGRAM id=$PBS_ARRAYID $PROGRAM_ARGUMENTS output file=$STAN_PROGRAM_BASENAME-$PBS_ARRAYID.csv
 
 echo 'Done running'
